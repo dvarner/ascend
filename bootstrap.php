19245681a8630dff60f067cbfc1b510db86c004b
David Varner <dvarner@ascendphp.com>
2017-05-25 21:35:33 -0400

    Bootstrap update, composer.json update

--- bootstrap.php
+++ bootstrap.php
@@ -9,12 +9,15 @@
  * These are static Classes required to be manually defined
  */
 
+require_once __DIR__ . '/vendor/autoload.php';
+/*
 require_once __DIR__ . '/fw/_helper_functions.php';
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/fw/Debug.php';
 require_once __DIR__ . '/fw/BootStrap.php';
 require_once __DIR__ . '/fw/Request.php';
 require_once __DIR__ . '/fw/Route.php';
+*/
 
 use \Ascend\BootStrap as BS;
 
